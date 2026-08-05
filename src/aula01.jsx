3afd4cb5b7892b8075e0d4e894390e049e5b5f55
Kelvin José <josecanha453@gmail.com>
2026-08-04 22:12:40 -0300

    Add files via upload

--- src/aula01.jsx
+++ src/aula01.jsx
@@ -4,7 +4,11 @@ import {View, Text, StyleSheet} from 'react-native';
 class Aula01 extends Component {
     render() {
         return (
-
+            <View>
+                <Text>Estrutura Padrão</Text>
+            </View>
         )
     }
 }
+
+export default Aula01;
