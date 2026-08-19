9c30432bb7acda8c9208a8bba63671b54e60a649
Kelvin José <josecanha453@gmail.com>
2026-08-18 22:15:22 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Checkbox } from 'expo-checkbox';
 import { 
   View, 
   Text, 
@@ -9,11 +10,25 @@ import {
 } from 'react-native';
 
 class Aula05 extends Component {
+
+  //CONSTRUTOR - Forma tradicional de inicializar estado (dados)
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nome: '',
+      email: '',
+      lembrarSenha: false
+    }
+  }
+
   render() {
     return (
         <View style={styles.container}>
         
             <View>
+
+              <Text style={styles.titulo}>Login</Text>
 
                 <Image
                     source={require('../img/logo_barao.png')}
@@ -31,12 +46,24 @@ class Aula05 extends Component {
                         placeholder='     Informe seu e-mail:'
                     />
 
+                    <View style={{flexDirection: 'row', alignItems: 'center', justifyContent:'center'}}>
+                      <View style={{flex: 1, flexDirection:'row'}}>
+                        <Checkbox
+                          value={this.state.lembrarSenha }
+                          onValueChange={ (valor) => this.setState({lembrarSenha: valor})}
+                          color={ this.state.lembrarSenha ? '#4630EB': undefined}
+                        />
+                        <Text style={{color:'white', marginLeft: 3, fontSize: 12}}>Lembrar senha</Text>
+                      </View>
+                      <Text style={{color: '#39FF14', fontSize: 12,}}>Esqueceu a senha?</Text>
+                    </View>
+
                     <TouchableOpacity style={styles.botao}>
                       <Text style={ styles.textoBotao}>Entrar</Text>
                     </TouchableOpacity>
 
-                    <Text style={styles.texto1}>Não tem conta ...
-                      <Text style={styles.texto2}>Cadastre-se!</Text>
+                    <Text style={styles.texto1}>Não tem conta ?
+                      <Text style={styles.texto2}> Cadastre-se!</Text>
                     </Text>
 
             </View>
@@ -77,11 +104,12 @@ const styles = StyleSheet.create({
     width: 120,
     height: 120,
     alignSelf: 'center',
-    marginBottom: 70,
+    marginBottom: 50,
+    marginTop: 20,
   },
 
   botao: {
-    backgroundColor: 'green',
+    backgroundColor: '#39FF14',
     width: 250,
     padding: 10,
     borderRadius: 5,
@@ -95,10 +123,23 @@ const styles = StyleSheet.create({
 
   texto1: {
     color: 'white',
+    fontSize: 12,
+    marginTop: 6,
+    textAlign: 'center',
   },
 
   texto2: {
-    color: 'blue',
+    color: '#39FF14',
+    //marginRight: 20,
+    fontSize: 12,
+    marginLeft: 10,
+  },
+  titulo: {
+    color: '#39FF14',
+     fontSize: 50,
+    marginTop: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
 
