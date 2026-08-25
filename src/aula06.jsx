fb06d24eaa1953bbe5eb196bd6adcece2b6d341a
Kelvin José <josecanha453@gmail.com>
2026-08-25 20:16:29 -0300

    Add files via upload

--- src/aula06.jsx
+++ src/aula06.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  ScrollView,
+  Button } 
+  from 'react-native';
 
 class Aula06 extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      nome: 'Kelvin',
+      sobrenome:'', 
+    }
+
+    this.mudarNome = this.mudarNome.bind(this);
+
+  }
+
+  mudarNome() {
+    this.setState({
+      nome: 'Ronaldinho',
+      sobrenome: 'José Canha',
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text></Text>
+        <ScrollView>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Alias, nam? Id blanditiis inventore praesentium. 
+            Molestiae inventore nihil hic vero! 
+            At eligendi commodi nam expedita dolore molestiae sit optio debitis eaque!
+          </Text>
+
+          <Text style={{fontSize: 21, color: 'red', marginTop: 50}}>
+            {this.state.nome } {this.state.sobrenome}
+          </Text>
+
+          <Button
+          title='Mostrar o Nome'
+          onPress={ this.mudarNome}
+          />
+
+        </ScrollView>
       </View>
     );
   }
@@ -20,3 +63,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
+
+//colocar o sobre nome e fazer aparecer no click do button
